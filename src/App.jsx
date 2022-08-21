8f38f512d67541245dfcbbdc150412ace2f8a505
Dishant Sharma <dishu5570@gmail.com>
2022-08-21 16:08:49 +0530

    tnc

---
+++ src/App.jsx
@@ -0,0 +1,132 @@
+import React from 'react'
+import logo from './assets/logo.png'
+const App = () => {
+  return (
+    <div>
+       <header className="main-header dark-theme" style={{display: 'flex', justifyContent: 'center', alignItems: 'center', position:'fixed', top:'0'}}>
+
+    <a href="https://scalant.in" target="_blank"><img src={logo} style={{height:'3rem'}} /></a>
+  </header>
+  
+  <section id="terms-of-service">
+    <div className="card">
+      <h1 className="primary-heading">Terms of Service</h1>
+      <p className="paragraph">
+      These terms and conditions apply to all the team members of Scalant, regardless of position or domain. We hope this will help to ensure that the community is valuable to you and serves as a platform for our collective growth. These T&Cs also safeguard the rights of every member, as well as our standing and reputation.{" "}<br/> 
+        <br />
+        <div className="bold">Registration Obligations</div><br/>
+      1.{" "}For community and privacy purposes, we would need your basic information.<br/>
+      2.{" "}Provide Scalant with authentic, accurate, up-to-date, and comprehensive information about yourself whenever required.<br/>
+      3.{" "}Scalant reserves the right to suspend or terminate your application if you provide any information that is inaccurate, out-of-date, or incomplete, or if Scalant has reasonable grounds to suspect that such information is fake or misleading.<br/>
+      4.{" "}If necessary, we may request identity verification for security reasons.<br/>
+        <br />
+        <br />
+        <div className="bold">General</div><br/>
+        1. To uphold the integrity and sovereignty of our community, our work process and organisational culture must remain private among us.
+        <br />
+        <br/>
+        <div style={{marginLeft:'1rem'}}>
+        a. You are not permitted to post, speak, or write on Scalant's behalf until you receive a specific request to do so.<br/>
+        b. If the hierarchy gives you the go-ahead, you might only share specific information.<br/>
+        c. You are not permitted to reveal any information about our upcoming events and activities until they have been officially announced.<br/>
+        d. Any disclosure of internal team processes or discussions is strictly prohibited.<br/>
+        e. Scalant reserves the right to impose strict measures against you, which may include dismissing you from the team, if it is discovered that you have disclosed unapproved information about our community.<br/>
+        </div>
+        <br />
+
+        2. If you choose to contribute as part of a team, you must commit to staying with us for at least three months after joining, unless you are expelled or there are unavoidable circumstances.<br/><br/>
+        3. Follow all of the guidelines and instructions that will be provided to you on a regular basis.<br/><br/>
+        4. If you wish to take a leave for a few days, send an email with your reasons to scalantofficial@gmail.com. We can then review your situation and decide whether to grant you full-time or part-time leave.<br/><br/>
+        5. Keep the discussions civil and objective. Any form of disrespectful or impolite behavior, religious or political dispute, hatred, discrimination, or legally dubious behaviour is not encouraged by Scalant. If anyone is found to be guilty of the same, strict measures will be taken against them.<br/><br/>
+        6. Sharing any kind of offensive or adult content is not supported by us. If anyone is found to be doing so, strict and immediate measures may be taken against them.<br/><br/>
+        <br />
+        <div className="bold">Privacy Policy</div><br/>
+        <div  style={{marginLeft:'1rem'}}>
+        a. You are <span className="bold">not</span> permitted to:
+        <br />
+        <br/>
+        <div style={{marginLeft:'2rem'}}>
+        a. sell any rewards, gifts, or swags that are owned by the Scalant.<br/>
+        b. share screenshots, recordings, or forward messages of our team discussions anywhere outside.<br/>
+        c. stream or broadcast our events, sessions, meetings, or any other team activity or conversation.<br/>
+        d. share any team members' personal information anywhere without their consent.<br/>
+        e. delete or upload any content to our official social media handles until and unless directed to.<br/>
+        f. duplicate context, sell data, or reproduce without permission.<br/>
+        </div>
+        <br/>
+        b. Scalant does not take any responsibility for your private interactions among the team members. In the event of any misuse of your personal information or any form of harassment, please report it to our official email address.     
+        <br />
+        <br />
+        </div>
+        7. Third-party policies: Scalant has no control over the services or websites that belong to third parties. The availability of any third-party site or product does not imply our endorsement, support, or warranty of that third-party site or product. Third-party sites and products are only made available to you as a convenience.
+        <br />
+        <br />
+        8. You are strictly prohibited from promoting your personal content in the community, unless it fits the context of a conversation going on in the community or you have been allowed to share it by the team. This includes: sharing links to your twitter, linkedin, or instagram posts; sharing links to events hosted by other communities that have not been approved by the team; and sharing links to your blog posts or youtube videos.
+        <br />
+        <br/>
+        However, this does not apply to projects that you have open-sourced on github/gitlab/bitbucket, provided that you are sharing them in order to gain more contributors or feedback on the project.
+        <br/>
+        <br />
+        9. We are not obligated to subsidise any costs associated with your personal purchases of any app/website/tools subscription in order to contribute to Scalant.
+        <br/>
+        <br />
+        10. Scalant is not liable for any collateral damage caused to the team members during work hours.
+        <br/><br/>
+        11. Our community is currently operating remotely, and we do not have set work hours. You are free to work at your preferred times as long as you satisfy our deadlines and remain engaged.
+        <br/><br/>
+        12. You are fully responsible for any damage you cause to Scalant, team members, or their private property, or to any member of the community, and you may face penalties as a result.
+        <br/><br/>
+        13. As a member of the team, you are expected to carry out certain duties in order to contribute to the community. Cooperate with the team in the event of any unfavourable circumstance to ensure effective workflow and contributions.
+        <br/><br/>
+        14. We regret having to say that Scalant is under no obligation to compensate you financially for your work/contributions. We may, however, intend to provide financial assistance to our devoted team members in the future, depending on the development and revenue of our community.
+        <br/><br/>
+        15. We are not liable to reimburse any team member who voluntarily provides financial support for Scalant at any time.
+        <br/><br/>
+        16. Despite the fact that you are free to work with any other organisations or communities that are similar to ours while you are working with us, we may ask about your role and contributions there in order to avoid any potential conflicts.
+        <br/><br/>
+        <div className="bold">Content Rights</div><br/>
+        1. You provide us with all the rights, authorizations, and licences required to use, display, distribute, and otherwise make use of your content in connection with the Scalant Community. In our sole discretion, we reserve the right to modify or remove your content, as well as to restrict or disable comments on a topic.<br/><br/>
+
+        2. You also grant us the right to re-use your content to market and promote Scalant, such as on our website, in public or private presentations, or in connection with other publicity efforts, even if you leave the community. It is forbidden to claim ownership of any content of Scalant, or share it as your own, even if you have contributed to it.<br/><br/>
+
+        3. In the case of code contributions, where you’re completely designing a project for Scalant, you would need to use a permissive open source license. If you want to claim complete ownership of a project, you may be able to do so after a few discussions with the technical team.<br/><br/>
+
+        4. If you suspect that your content is being misused, please contact us via email. We will look into it further and do our best to resolve the issue.<br/><br/>
+
+        <div className="bold">Termination of Position</div><br/>
+        1. Scalant has the authority to terminate your position in the following situations: <br/><br/>
+        <div  style={{marginLeft:'2rem'}}>
+        a. If you violate the terms and conditions,<br/>
+        b. If there is any behaviour that is disrespectful to our team or to the community,<br/>
+        c. If negative or toxic behaviour is observed on your LinkedIn, Twitter, or other social platforms, or at any in-person events,<br/>
+        d. If you remain inactive for no apparent reason despite receiving two warnings, Scalant may decide not to proceed with you after the third.<br/>
+        </div>
+        <br/>
+        2. If a person intends to leave the team voluntarily, <br/><br/>
+        <div style={{marginLeft:'2rem'}}>
+        a. It is their responsibility to complete the work they have been assigned or have chosen to do before departing from the team so that the rest of the team is not inconvenienced.<br/>
+        b. They must send an email to our official email ID with the appropriate justification to the authorities seven days before they plan to depart.<br/>
+        c. After they send their email of resignation and it is accepted, they will still be a part of the team and work with us for the following seven days.<br/>
+        </div>
+        <br/>
+        <span className='bold'>Note:</span> To ensure that we can work effectively as a team, we will continuously monitor our workflow and management and, if necessary, update our policies further after consulting with the team. If you do not agree with any particular point and have a solid reason to disagree, or you think we have missed any perspective to consider, please email us about the same so that we can discuss it further.
+        <br/><br/>
+        To contact us, email us at scalantofficial@gmail.com
+
+      </p>
+    </div>
+  </section>
+  <div style={{display: 'flex', justifyContent: 'center', gap:'2rem'}}>
+   <a href="mailto:scalantofficial@gmail.com?subject=Welcome Letter Acceptance&body=I have read and accept the terms and conditions, hence I accept the welcome letter."><button className='acptBtn main-header dark-theme'>Accept</button></a>
+   <a href="mailto:scalantofficial@gmail.com?subject=Welcome Letter Acceptance&body=I have read and accept the terms and conditions, hence I accept the welcome letter."><button className='acptBtn header-mobile dark-theme'>Accept</button></a>
+   </div>
+  <footer>
+    <p className="footer-heading">
+      © Copyright 2022-2023 Scalant. All rights reserved.
+    </p>
+  </footer>
+    </div>
+  )
+}
+
+export default App
